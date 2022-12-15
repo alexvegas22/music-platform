d251518d547ac44f8fcd6cb5550f5e11a793b2f0
Alex Vegas <abaudouinvegas@git.dti.crosemont.quebec>
2022-12-15 12:58:29 -0500

    changed structure

---
+++ profile.inc.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="css\style.css">
+        <link rel="stylesheet" href="css\profile.css">
+</head>
+<body>
+        <div class="grid-container">
+                <img class="item1" src="images\user.png">
+                <div class="item2">User</div>
+                <div class ="item3">Log out</div>
+
+                <div class="item4">Follows</div>
+                <div class="item5">Followers</div>
+                <div class="item6">Liked Songs</div>
+                <div class="item7">Playlists</div>
+        </div>
+</body>
+</html>
--- song.php
+++ song.php
@@ -5,10 +5,25 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css\style.css">
-    <title>Song Page</title>
+    <link rel="stylesheet" href="css\song.css">
+    
 </head>
 <body>
-<?php include 'navbar.inc.php';
-    ?>
+    <div class="song-box">
+        <img class="album" src="https://upload.wikimedia.org/wikipedia/commons/3/3c/No-album-art.png" height=150px width=150px>
+        <div class="title">Title</div>
+        <div class="artist">Artist</div>
+        <div class= "row">
+        <img src="https://img.icons8.com/ios-filled/50/null/skip-to-start--v3.png"/>
+        <img src="https://img.icons8.com/ios-filled/50/null/play--v2.png"/>
+        <img src="https://img.icons8.com/ios-filled/50/null/end--v1.png"/>
+        </div>
+    </div>
+    <audio controls>
+  
+  <source src="Inna pad.wav" type="audio/x-wav">
+Your browser does not support the audio element.
+</audio>
+
 </body>
 </html>
