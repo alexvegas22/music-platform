b127322339d284cacc2c3891f9b69ed8e1ad4d83
Alex Vegas <abaudouinvegas@git.dti.crosemont.quebec>
2022-12-15 17:33:50 -0500

    sidebar

--- song.php
+++ song.php
@@ -14,16 +14,42 @@
         <div class="title">Title</div>
         <div class="artist">Artist</div>
         <div class= "row">
-        <img src="https://img.icons8.com/ios-filled/50/null/skip-to-start--v3.png"/>
-        <img src="https://img.icons8.com/ios-filled/50/null/play--v2.png"/>
+        <img onclick="restart()" src="https://img.icons8.com/ios-filled/50/null/skip-to-start--v3.png"/>
+        <img id="play" onclick="button()" src="https://img.icons8.com/ios-filled/50/null/play--v2.png">
         <img src="https://img.icons8.com/ios-filled/50/null/end--v1.png"/>
         </div>
     </div>
-    <audio controls>
-  
-  <source src="Inna pad.wav" type="audio/x-wav">
-Your browser does not support the audio element.
-</audio>
+    <audio id="song">
+        <source src="music/Inna pad.wav" type="audio/wav" >
+    </audio>
+
+
+    <script> 
+    var play = "https://img.icons8.com/ios-filled/50/null/play--v2.png";
+    var pause = "https://img.icons8.com/ios-filled/50/null/pause--v1.png";
+    var state = 1;
+    var queue = [];
+    var current_song = document.getElementById("song"); 
+    var previous_song = current_song;
+    var next_song ;
+    function restart(){
+        previous_song.load();
+        previous_song.play();
+    }
+    function button(){
+        if (state==1){
+            document.getElementById("play").src="https://img.icons8.com/ios-filled/50/null/pause--v1.png";
+            current_song.play();
+            state=2;
+        }   
+    
+        else if(state==2){
+            document.getElementById("play").src="https://img.icons8.com/ios-filled/50/null/play--v2.png";
+            current_song.pause();
+            state=1;
+        }    
+    }
+</script>
 
 </body>
 </html>
