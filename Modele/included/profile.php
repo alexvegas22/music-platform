b1107bf93528a29d193324d4964a2c78caf8e8d4
Alex Vegas <abaudouinvegas@git.dti.crosemont.quebec>
2022-12-21 15:10:40 -0500

    session

--- Modele/included/profile.php
+++ Modele/included/profile.php
@@ -1,32 +1,24 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="css\style.css">
-        <link rel="stylesheet" href="css\profile.css">
-        
-    <script src="scripts/jquery-3.6.2.js"></script>
-    <script src="scripts/user-section.js"></script>
-</head>
-<?php include("Modele\classes\profile.class.php");
-$user = new Profile("Alex", "alexvegas", "root", "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/SNice.svg/1200px-SNice.svg.png" );?>
-<body onload="checkUser()">
-        <div id = "profile-grid" class="grid-container">
-                <img class="item1" src="<?= $user->getPhoto() ?>">
-                <div class="item2"><?= $user->getNom() ?></div>
-                <div id="logout" class ="item3">Log out</div>
+<?php 
+include("Modele\classes\profile.class.php");
 
-                <a class="item4" href="songPage.php"> My playlists</a> 
-                <a class="item5" href="songPage.php"> Liked Songs</a> 
-                <a href="songPage.php" class="item6">Liked Playlists</a> 
-                <a href="songPage.php" class="item7">My profile</a> 
-        </div>
-        <div id="guest-grid" class="guest">
-                <div id='login'>Log in</div>
-                <div id='signup'>Sign up</div>
-        </div>
-</body>
+function afficherProfil($profil){
+        echo "<div id = 'profile-grid' class='grid-container'>";
+        echo "<img class='item1' src='".$profil->getPhoto()."'>";
+        echo "<div class='item2'>".$profil->getNom()."</div>";
+        echo "<div id='logout' class ='item3'>Log out</div>";
+        echo "<a class='item4' href='songPage.php'> My playlists</a>";
+        echo "<a class='item5' href='songPage.php'> Liked Songs</a>"; 
+        echo "<a href='songPage.php' class'item6'>Liked Playlists</a>";
+        echo "<a href='songPage.php' class='item7'>My profile</a> ";
+        echo "</div>";
 
-</html>
+}
+
+function afficherGuest(){
+        echo "<div id='guest-grid' class='guest'>
+        <div id='login'>Log in</div>
+        <div id='signup'>Sign up</div>
+        </div>";
+}
+
+?>
--- Modele/included/sidebar.php
+++ Modele/included/sidebar.php
@@ -1,3 +1,5 @@
+<?php session_start();?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,6 +10,7 @@
     
     <link rel="stylesheet" href="css\sidebar.css">
     <link rel="stylesheet" href="css\style.css">
+    <link rel="stylesheet" href="css\profile.css">
 </head>
 <body>
 <div class = "background">
@@ -19,8 +22,24 @@
             <a href="songPage.php"> Search</a> 
             
         </div>
-        <?php include 'Modele\included\profile.php';?>
-        <?php include 'Modele\included\musicPlayer.php';?>
+        <?php
+        
+        $_SESSION["username"]="admin";
+        include 'Modele\included\profile.php';
+        include 'Modele\classes\userDOA.class.php';
+         
+        // $profil = new Profile("Alex", "alexvegas", "root", "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/SNice.svg/1200px-SNice.svg.png" );
+
+         if (isset($_SESSION['user'])){
+            $req = new userRequest();
+            $profil = $req->getUser($_SESSION['user']);
+            afficherProfil($profil);
+         }else{
+            afficherGuest();
+         }
+         
+         
+         include 'Modele\included\musicPlayer.php';?>
     </div>   
 </div>
 </body>
